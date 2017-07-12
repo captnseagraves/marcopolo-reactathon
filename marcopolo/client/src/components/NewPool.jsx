340f8b9498ecb72ec77880b76b0c10563f47228d
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-07-12 12:22:23 -0600

    Form added, need form styling

--- marcopolo/client/src/components/NewPool.jsx
+++ marcopolo/client/src/components/NewPool.jsx
@@ -1,20 +1,58 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import '../NewPool.css';
 
 class NewPool extends Component {
   render() {
     return (
       <form className="poolForm" name="poolForm">
-        <label>Pool Name</label>
-          <input type="text"></input>
-        <label>Pool Location</label>
-          <input type="text"></input>
-        <label>Pool Description</label>
-          <textarea></textarea>
-        <label>Pool Picture</label>
-          <input type="text"></input>
+        <div className="input-group text-left">
+          <label>Pool Name</label>
+          <br />
+          <input type="text" className="form-control poolInput" />
+        </div>
+        <br />
+        <div className="input-group text-left">
+          <label>Pool Location</label>
+          <br />
+          <input type="text" className="form-control poolInput" />
+        </div>
+        <br />
+        <div className="input-group text-left">
+          <label>Pool Description</label>
+          <br />
+          <input type="text" className="form-control poolInput" />
+        </div>
+        <br />
+        <div className="input-group text-left">
+          <label>Pool Picture</label>
+          <br />
+          <input type="text" className="form-control poolInput" />
+        </div>
       </form>
-    );
+    )
   }
 }
 
+
 export default NewPool;
+{/* <form className="poolForm" name="poolForm">
+  <label className="row text-left">Pool Name
+    <br/>
+    <input type="text" className="col-xs-12"></input>
+
+  <br/>
+  <label className="row text-left">Pool Location
+    <br/>
+    <input type="text" className="col-xs-12"></input>
+  </label>
+  <br/>
+  <label className="row text-left">Pool Description
+    <br/>
+    <textarea className="col-xs-12"></textarea>
+  </label>
+  <br/>
+  <label className="row text-left">Pool Picture
+    <br/>
+    <input type="text" className="col-xs-12"></input>
+  </label>
+</form> */}
