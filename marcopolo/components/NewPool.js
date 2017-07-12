ee2cce3e7882da92a44b0fdb86566cf56da3f412
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-07-12 11:05:10 -0600

    Add skeleton components

---
+++ marcopolo/components/NewPool.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NewPool extends Component {
+  render() {
+    return (
+      <div className="poolForm">
+      </div>
+    );
+  }
+}
+
+export default NewPool;
